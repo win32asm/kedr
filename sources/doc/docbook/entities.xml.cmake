b811ff69593e9a779d60a368368fa52176472969
Tsyvarev Andrey <tsyvarev@ispras.ru>
2010-07-19 14:21:04 +0400

    Added script kedr_capture_trace_until_done.
    Capture trace, until it encounter message, that target module is unloaded.

---
+++ sources/doc/docbook/entities.xml.cmake
@@ -0,0 +1,21 @@
+<!-- ISPRAS -->
+<!ENTITY ispras-name "Institute for System Programming of the Russian Academy of Sciences (ISPRAS)">
+<!ENTITY ispras-url "http://www.ispras.ru/en/">
+
+<!-- release- and version-related stuff -->
+<!ENTITY rel-version "@KEDR_VERSION_MAJOR@.@KEDR_VERSION_MINOR@">
+<!ENTITY rel-date    "November 21, 2010">
+<!ENTITY kedr-lifespan   "2010">
+
+<!-- email addresses -->
+<!ENTITY email-spectre "spectre@ispras.ru">
+<!ENTITY email-tsyvarev "tsyvarev@ispras.ru">
+
+<!ENTITY email-linux   "linux@ispras.ru">
+
+<!-- various URLs  -->
+<!ENTITY kedr-url           "https://developer.berlios.de/projects/kedr/">
+<!ENTITY kernel-url         "http://kernel.org/">
+
+<!-- service script -->
+<!ENTITY kedr-service        "kedr">
---
+++ sources/doc/docbook/main.xml.cmake
@@ -0,0 +1,70 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE article PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN"
+    "@DOCBOOK_DTD_FILE@" [ 
+        <!ENTITY % kedr-entities SYSTEM "entities.xml"> %kedr-entities; 
+        <!ENTITY kedr_service SYSTEM "control.xml">
+        <!ENTITY capture_trace SYSTEM "capture_trace.xml">
+        <!ENTITY kedr_intro SYSTEM "intro.xml">
+        <!ENTITY kedr_call_mon SYSTEM "call_mon.xml">
+        <!ENTITY kedr_extend SYSTEM "extend.xml">
+        <!ENTITY kedr_fault_sim SYSTEM "fault_sim.xml">
+        <!ENTITY kedr_getting_started SYSTEM "getting_started.xml">
+        <!ENTITY kedr_install SYSTEM "install.xml">
+        <!ENTITY kedr_overview SYSTEM "overview.xml">
+        <!ENTITY kedr_tips SYSTEM "tips.xml">
+    ]
+>
+
+<article lang="en">
+<title>KEDR &rel-version; Reference Manual</title>
+<articleinfo>
+    <releaseinfo>KEDR &rel-version; Reference Manual (&rel-date;)</releaseinfo>
+    <authorgroup>
+	    <author>
+	        <firstname>Eugene</firstname>
+	        <surname>Shatokhin</surname>
+	        <email>spectre@ispras.ru</email>
+	    </author>
+	    <author>
+	        <firstname>Andrey</firstname>
+	        <surname>Tsyvarev</surname>
+	        <email>tsyvarev@ispras.ru</email>
+	    </author>
+    </authorgroup>
+    <copyright>
+        <year>&kedr-lifespan;</year>
+        <holder>&ispras-name;</holder>
+    </copyright>
+    <legalnotice>
+        <para><ulink url="&ispras-url;">&ispras-url;</ulink></para>
+    </legalnotice>
+</articleinfo>
+
+<!-- ============ Top-level sections ============ -->
+<!-- Introduction -->
+&kedr_intro;
+
+<!-- Overview -->
+&kedr_overview; 
+
+<!-- Getting Started -->
+&kedr_getting_started;
+
+<!-- Call Monitoring -->
+&kedr_call_mon;
+
+<!-- Fault Simulation -->
+&kedr_fault_sim;
+
+<!-- Extending KEDR -->
+&kedr_extend;
+
+<!-- Tips and Tricks -->
+&kedr_tips;
+
+<!-- Controlling KEDR -->
+&kedr_service;
+
+<!-- Capture trace -->
+&capture_trace;
+</article>
