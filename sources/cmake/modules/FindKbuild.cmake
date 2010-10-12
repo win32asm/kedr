e8ab0d2c9afea00ed79350e511d3b26f4ffed818
Tsyvarev Andrey <tsyvarev@ispras.ru>
2010-10-12 19:17:59 +0400

    Add 'pid' control file for fault simulation indicator.
    It will be filter only thouse processes, which derived from process with given pid.

---
+++ sources/cmake/modules/FindKbuild.cmake
@@ -0,0 +1,19 @@
+# Set variables:
+#
+# KBUILD_VERSION_STRING - `uname -r` (${CMAKE_SYSTEM_VERSION})
+# KBUILD_BUILD_DIR  -   directory, where module are build (/lib/modules/${KBUILD_VERSION_STRING}/build)
+# KBUILD_INCLUDE_DIR - /usr/src/linux-headers-${KBUILD_VERSION_STRING}/include
+# KBUILD_FOUND - TRUE if all correct, FALSE otherwise
+
+set(KBUILD_VERSION_STRING ${CMAKE_SYSTEM_VERSION} CACHE STRING "Kernel version for which KEDR is builded.")
+set(KBUILD_BUILD_DIR "/lib/modules/${KBUILD_VERSION_STRING}/build")
+set(KBUILD_INCLUDE_DIRS "/usr/src/linux-headers-${KBUILD_VERSION_STRING}/include")
+
+# Note: only KBUILD_BUILD_DIR variable is really used in the project.
+# Others variables defined only for FindModule architecture of CMake.
+
+# Handle the QUIETLY and REQUIRED arguments and set KBUILD_FOUND to TRUE if 
+# all listed variables are TRUE
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Kbuild DEFAULT_MSG KBUILD_BUILD_DIR)
